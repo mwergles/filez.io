3c9afd82a4a24e9bc0ea14a1da7ce14e2a8f1fb2
Marcelo Wergles <mwergles@gmail.com>
2023-05-11 12:04:48 -0300

    Setup docker and installation

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+docker info > /dev/null 2>&1
+
+# Ensure that Docker is running...
+if [ $? -ne 0 ]; then
+    echo "Docker is not running."
+
+    exit 1
+fi
+
+docker run --rm \
+    --pull=always \
+    -v "$(pwd)":/opt \
+    -w /opt \
+    laravelsail/php82-composer:latest \
+    bash -c "composer install"
+
+./vendor/bin/sail build
+
+BOLD='\033[1m'
+NC='\033[0m'
+
+echo ""
+
+if sudo -n true 2>/dev/null; then
+    sudo chown -R $USER: .
+    echo -e "${BOLD}Get started with:${NC} ./vendor/bin/sail up"
+else
+    echo -e "${BOLD}Please provide your password so we can make some final adjustments to your application's permissions.${NC}"
+    echo ""
+    sudo chown -R $USER: .
+    echo ""
+    echo -e "${BOLD}Thank you! Dive in with:${NC} ./vendor/bin/sail up"
+fi
